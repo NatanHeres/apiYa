cf1eaa2fb35dd80ad928ea3348c361e5140cc050
Xyro-Dev <xyroteam110@gmail.com>
2024-07-01 15:03:36 +0700

    bismillah

--- install-deps.sh
+++ install-deps.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v apt-get)" ]; then
-  echo 'Error: apt-get is not installed.' >&2
-  exit 1
-fi
-
-apt-get update
-apt-get install -y \
-  libnss3 \
-  libnspr4 \
-  libgbm1
+# Install Playwright dependencies
+npx playwright install-deps
